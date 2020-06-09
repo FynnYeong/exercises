3e34c71a6ac2337db9e67cb3f4c1759595553434
yanghui <yanghui@wufeng-network.com>
2020-06-09 09:55:16 +0800

    解决重定向

--- GithubHotItemPro/src/components/App.jsx
+++ GithubHotItemPro/src/components/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { hot } from 'react-hot-loader/root';
-import { HashRouter as Router, Route,NavLink,Redirect } from 'react-router-dom'
+import { HashRouter as Router, Route,NavLink } from 'react-router-dom'
 import Popoular from "../pages/Popoular"
 import Battle from "../pages/Battle"
 
@@ -20,7 +20,7 @@ class App extends React.Component {
           <NavLink onClick={()=>{this.setState({linkKey:1})}} className={`a ${this.state.linkKey===1?"active2":null}`} to="/battle/0">Battle</NavLink>
         </div>
         
-        <Redirect path="/" to="/popoular" />
+        {/* <Redirect path="/" to="/popoular" /> */}
         {/* <Route exact path="/" component={Popoular} /> */}
         <Route exact path="/popoular/:key?/:src?" component={Popoular} />
         <Route path="/battle/:open?/:user1?/:user2?" component={Battle} />
